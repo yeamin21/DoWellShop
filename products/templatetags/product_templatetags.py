5cc52677cfac2d4eadc18f9a9f1c5d97c84154f3
yeamin21 <yeamin21@outlook.com>
2021-02-17 16:22:01 +0600

    added categories in search options

--- products/templatetags/product_templatetags.py
+++ products/templatetags/product_templatetags.py
@@ -5,9 +5,9 @@ from products.models import Category, Product, Manufacturer
 register = template.Library()
 
 
-@register.inclusion_tag('products/category_list.html')
-def categories():
-    return {'categories': Category.objects.all()}
+# @register.inclusion_tag('products/category_list.html')
+# def categories():
+#     return {'categories': Category.objects.all()}
 
 
 @register.inclusion_tag('products/manufacturer_list.html')
@@ -18,3 +18,8 @@ def manufacturers():
 @register.inclusion_tag('products/latestproduct_list.html')
 def products(count=None):
     return {'products': Product.objects.order_by('-time_added')[:count]}
+
+
+@register.inclusion_tag('users/dummy_template.html')
+def categories(template='products/category_list.html'):
+    return {'categories': Category.objects.all(), 'template':template}
